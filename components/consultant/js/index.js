83ac7909e57dbbb0d0f5f6ed2a815441248cd803
ledihduc <ducswan36@gmail.com>
2025-01-13 02:21:21 +0900

    add data

--- components/consultant/js/index.js
+++ components/consultant/js/index.js
@@ -1,32 +1,30 @@
-fetch('/components/consultant/data/index.json')
-  .then(response => response.json())
-  .then(data => {
-
+fetch("/components/consultant/data/index.json")
+  .then((response) => response.json())
+  .then((data) => {
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
+    const id = urlParams.get("id");
 
-    const detailUserEl = document.querySelector('#detail_user')
-    const listRelatedEl = document.querySelector('#list_relative')
+    const detailUserEl = document.querySelector("#detail_user");
+    const listRelatedEl = document.querySelector("#list_relative");
 
     data.forEach((user) => {
-      if (user?.id == id) {
-        showDetailUser(detailUserEl, user)
+      if ((user?.id == id) & (user?.tab === "PROJECT")) {
+        showDetailUser(detailUserEl, user);
       }
-    })
+    });
 
-    showListRelated(listRelatedEl, data)
+    showListRelated(listRelatedEl, data);
 
-    detailUserEl
+    detailUserEl;
   })
-  .catch(err => console.log(err));
-
+  .catch((err) => console.log(err));
 
 function showDetailUser(el, user) {
-  el.innerHTML = ''
-  let html = ''
+  el.innerHTML = "";
+  let html = "";
   html += `
     <div class="flex-box wrap">
-        <figure><img src="${user.image}" alt="${user.name}"></figure>
+        <figure><img src="${user.thumbnail}" alt="${user.thumbnail}"></figure>
         <div>
             <p class="name">
                 ${user.name1}
@@ -39,14 +37,14 @@ function showDetailUser(el, user) {
             <p>保有資格: ${user.reward}</p>
         </div>
     </div>
-  `
+  `;
 
-  el.innerHTML = html
+  el.innerHTML = html;
 }
 
 function showListRelated(el, data) {
-  el.innerHTML = ''
-  let html = ''
+  el.innerHTML = "";
+  let html = "";
   for (let i = 0; i < data.length; i++) {
     html += `
       <a href="consultant.html?id=${data[i].id}" class="consultant-content__item" style="color: inherit;">
@@ -55,7 +53,7 @@ function showListRelated(el, data) {
           </div>
           <p class="name">${data[i].name2}</p>
       </a>
-    `
+    `;
   }
-  el.innerHTML = html
+  el.innerHTML = html;
 }
